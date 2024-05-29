f6dd626f9b50c0573dcc88bbf89ddf1a383fed08
Nick-Nunley <nnunley@mednet.ucla.edu>
2024-05-28 17:48:44 -0700

    Added 'E' and 'F' technical replicates to sample-IDs

--- Harvest_05.28.2024/generate_ids.py
+++ Harvest_05.28.2024/generate_ids.py
@@ -3,7 +3,7 @@ import json
 mouse_phenos = ['FUCRW', 'MYCN', 'IDH1', 'MYCN;IDH1']
 number_of_mice = 2
 tumor_num = 2
-tech_reps = 4
+tech_reps = 6
 
 sample_json = {}
 for pheno in mouse_phenos:
@@ -19,6 +19,12 @@ for pheno in mouse_phenos:
                     sample_json_key = f'{sample_json_key}-C'
                 elif rep == 3:
                     sample_json_key = f'{sample_json_key}-D'
+                elif rep == 4:
+                    sample_json_key = f'{sample_json_key}-E'
+                elif rep == 5:
+                    sample_json_key = f'{sample_json_key}-F'
+                else:
+                    raise IndexError(f'More technical reps were specified than can be handled')
                 sample_json[sample_json_key] = ''
 
 with open('Harvest_05.28.2024/sample_ids.json', 'w', encoding = 'UTF-8') as json_file:
