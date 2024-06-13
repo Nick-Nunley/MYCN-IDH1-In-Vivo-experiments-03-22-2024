48ce8cf6a6dcd835322268ef903dd992681e39cf
Nick-Nunley <nnunley@mednet.ucla.edu>
2024-06-12 17:03:35 -0700

    Initial commit of adjusted tumor BCA

--- Western_blot/BCA/BCA_execution.sh
+++ Western_blot/BCA/BCA_execution.sh
@@ -2,8 +2,10 @@
 
 python Submodules/BCA-Assay/BCA_assay.py \
 -s Western_blot/BCA/standards_data.xlsx \
--u Western_blot/BCA/unknown_data.xlsx \
+-u Western_blot/BCA/unknown_data_subset.xlsx \
 -p 10.0 \
 -a \
 -f 4.5 \
--o Western_blot/BCA/bca_output.csv \
+-o Western_blot/BCA/bca_output_normalized_to_actin.csv \
+-S \
+-n Western_blot/Rep0/BCA_scaling_vector.csv
